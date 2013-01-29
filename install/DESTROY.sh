ad160adbd975d8070fd8f2fe09b8db022b234746
Marin Usalj <mneorr@gmail.com>
2013-01-29 10:57:04 -0800

    deleted databases

--- install/DESTROY.sh
+++ install/DESTROY.sh
@@ -9,6 +9,9 @@ sudo dscl . delete /Users/gitlab
 sudo rm -rf /Users/git /Users/git
 sudo rm -rf /Users/git /Users/gitlab
 
+dropdb gitlabhq_production
+dropuser gitlab
+
 # brew uninstall postgres
 # brew uninstall redis
 
